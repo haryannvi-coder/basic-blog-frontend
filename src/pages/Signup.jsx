96558e2234b19fe428034990b95608b9660e1cc7
Rohit Rao <130357830+haryannvi-coder@users.noreply.github.com>
2025-03-10 13:35:42 +0530

    Update Signup.jsx
    Chsnged backrnd url

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -42,7 +42,7 @@ export function Signup(){
 
             <Button onClick={async () => {
                 try {
-                    const res = await axios.post(`https://basic-blog-backend-production.up.railway.app/api/v1/user/signup`, {
+                    const res = await axios.post(`https://basic-blog-backend.onrender.com/api/v1/user/signup`, {
                         firstName, 
                         lastName,
                         email,
